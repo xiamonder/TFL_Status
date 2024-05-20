5e2c00b00c404f639f678956435b8f394bd3f6c5
xiamonder <acldiamond@gmail.com>
2024-05-20 17:37:49 +0100

    created api util functions

--- src/AllLinesPage/AllLinesPage.tsx
+++ src/AllLinesPage/AllLinesPage.tsx
@@ -1,3 +1,12 @@
+import { useEffect } from "react";
+import { getAllLinesStatus } from "../api_utils";
+
 export const AllLinesPage = () => {
+  useEffect(() => {
+    getAllLinesStatus().then((lineStatus) => {
+      console.log(lineStatus);
+    });
+  }, []);
+
   return <h1>All Lines</h1>;
 };
--- src/SingleLinePage/SingleLinePage.tsx
+++ src/SingleLinePage/SingleLinePage.tsx
@@ -1,3 +1,14 @@
+import { useEffect } from "react";
+import { getLineStatus } from "../api_utils";
+import { useParams } from "react-router-dom";
+
+type Params = { tubeLine: string };
 export const SingleLinePage = () => {
+  const { tubeLine } = useParams<Params>();
+  useEffect(() => {
+    getLineStatus(tubeLine!).then((lineStatus) => {
+      console.log(lineStatus);
+    });
+  }, [tubeLine]);
   return <h1>Single Line</h1>;
 };
